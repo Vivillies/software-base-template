7ab884f99f123f5954457978e4f0b59a847520b4
Vivillies <devvals.business@gmail.com>
2024-12-22 17:53:46 +0100

    feat: documented code

--- src/lib/database/database.ts
+++ src/lib/database/database.ts
@@ -1,5 +1,12 @@
+/**
+ * This file can be edited to fit your own needs.
+ * Please be mindful when making changes to this file as it is being used
+ * in multiple files, especially for authentication and authorization purposes.
+ * src/lib/auth/auth.ts
+ */ 
+
 import { MONGODB_URI, DATABASE_NAME } from "$env/static/private";
-import { Collection, MongoClient, type Document } from "mongodb";
+import { MongoClient } from "mongodb";
 
 const client: MongoClient = new MongoClient(MONGODB_URI);
 const database = client.db(DATABASE_NAME)
