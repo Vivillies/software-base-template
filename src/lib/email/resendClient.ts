3b920c96fed34e5e63fb2b4669ef49164f3b3a52
Vivillies <devvals.business@gmail.com>
2025-01-08 22:09:09 +0100

    feat: add resend email SDK

---
+++ src/lib/email/resendClient.ts
@@ -0,0 +1,21 @@
+import { RESEND_API_KEY } from "$env/static/private";
+import { Resend } from "resend";
+
+export const resend = new Resend(RESEND_API_KEY);
+
+export async function sendEmail(from: string, to: string[], subject: string, html: string){
+      const { data, error } = await resend.emails.send({
+            from: from,
+            to: to,
+            subject: subject,
+            html: html
+      })
+
+      // test
+
+      if (error) {
+            return console.error({ error });
+      }
+
+      console.log({ data });
+}
